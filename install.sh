5202df69cb2cda2b3b7eeee6ec41b0d0778dc27f
PixelRayn <david.kowalk@gmail.com>
2020-09-13 01:03:44 +0200

    Added feedback on linux install

--- install.sh
+++ install.sh
@@ -4,10 +4,18 @@
 # ===========================
 
 # Get current Directory
+echo "Finding Executable Directory..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "Adding alias to startup file..."
 STR="alias ams=\"python3 $DIR/src/ams_compiler.py\""
 echo $STR >> ~/.bashrc
 
 # Update Current Session
+echo "Update Current Session"
 alias ams="python3 $DIR/src/ams_compiler.py"
+
+echo "==========="
+echo "|| DONE! ||"
+echo "==========="
+echo ""
